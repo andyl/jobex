0f95af0c540399ec0b4a87d73f8fb1860eac2c75
andyl <andy@r210.com>
2019-08-19 12:26:12 -0700

    Add favicons

--- apps/crow_data/lib/crow_data/query.ex
+++ apps/crow_data/lib/crow_data/query.ex
@@ -1,27 +1,11 @@
 defmodule CrowData.Query do
-
   alias CrowData.Repo
   alias CrowData.Ctx.ObanJob
   import Ecto.Query
-
-  def all_jobs do
-    from(j in ObanJob, preload: [:results]) 
-    |> Repo.all()
-  end
+  import Modex.AltMap
 
   def job_states do
-      from(
-        j in ObanJob,
-        group_by: j.state,
-        select: {j.state, count(j.id)}
-      )
-      |> Repo.all()
-
-      default_states()
-  end
-
-  defp default_states do
-    %{
+    default_states = %{
       "executing" => 0,
       "available" => 0,
       "scheduled" => 0,
@@ -29,11 +13,64 @@ defmodule CrowData.Query do
       "discarded" => 0,
       "completed" => 0
     }
+
+    query_data =
+      from(
+        j in ObanJob,
+        group_by: j.state,
+        select: %{j.state => count(j.id)}
+      )
+      |> Repo.all()
+
+    merge_list(default_states, query_data)
   end
 
   def job_queues do
+    from(
+      j in ObanJob,
+      group_by: j.queue,
+      select: %{j.queue => count(j.id)}
+    )
+    |> Repo.all()
+    |> merge_list()
   end
 
   def job_types do
+    from(
+      j in ObanJob,
+      group_by: fragment("args -> 'type'"),
+      select: %{fragment("args -> 'type' as type") => count(j.id)}
+    )
+    |> Repo.all()
+    |> merge_list()
+  end
+
+  def job_query(uistate) do
+    case uistate do
+      %{field: nil, value: nil} -> jq_all() 
+      %{field: "state", value: state} -> jq_state(state) 
+      %{field: "queue", value: queue} -> jq_queue(queue) 
+      %{field: "type",  value: type}  -> jq_type(type) 
+    end |> Repo.all()
+  end
+
+  def job_all do
+    jq_all() |> Repo.all()
+  end
+
+  defp jq_all do
+    from(j in ObanJob, preload: [:results], order_by: j.id, limit: 50)
+  end
+
+  defp jq_state(state) do
+    from(j in jq_all(), where: j.state == ^state)
+  end
+
+  defp jq_queue(queue) do
+    from(j in jq_all(), where: j.queue == ^queue)
+  end
+
+  defp jq_type(type) do
+    from(j in jq_all(), where: fragment("args => 'type' = ?", ^type))
   end
 end
--- apps/crow_web/lib/crow_web/controllers/home_controller.ex
+++ apps/crow_web/lib/crow_web/controllers/home_controller.ex
@@ -2,7 +2,14 @@ defmodule CrowWeb.HomeController do
   use CrowWeb, :controller
 
   def index(conn, _params) do
-    render(conn, "index.html")
+    IO.inspect CrowData.Query.job_all()
+    conn
+    |> assign(:state_qry, CrowData.Query.job_states())
+    |> assign(:queue_qry, CrowData.Query.job_queues())
+    |> assign(:types_qry, CrowData.Query.job_types())
+    |> assign(:uistate,   %{field: nil, value: nil})
+    |> assign(:job_list,  CrowData.Query.job_all())
+    |> render("index.html")
   end
 
   def urls(conn, _params) do
