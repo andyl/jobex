5822f70b6a602b6df307b568e1751cb82eafc665
andyl <andy@r210.com>
2019-08-28 20:53:22 -0700

    Add sleep

--- rel/hooks/post_start/load_prod_jobs.sh
+++ rel/hooks/post_start/load_prod_jobs.sh
@@ -1,7 +1,11 @@
 #!/usr/bin/env sh
 
 echo LOADING PROD JOBS
-
 echo $RELEASE_ROOT_DIR
+echo SLEEPING 10 SECONDS
+
+sleep 10
+
+echo LOADING NOW!!!
 
 $RELEASE_ROOT_DIR/bin/crow rpc "CrowData.Scheduler.load_prod_jobs"
