1691c516fc22efd1ad142b6a55d94b3af239b1fd
andyl <andy@r210.com>
2019-09-22 19:48:09 -0700

    Update notes

--- apps/crow_data/lib/crow_data/runner/rambo.ex
+++ apps/crow_data/lib/crow_data/runner/rambo.ex
@@ -1,5 +1,13 @@
 defmodule CrowData.Runner.Rambo do
   def exec(cmd) do
-    Rambo.run(cmd)
+    cmd
+    |> String.split(";")
+    |> Enum.map(&(String.split(&1)))
+
+  end
+
+  defp run(cmd) do
+    cmd
+    |> Rambo.run()
   end
 end
