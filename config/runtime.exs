52280534801b2d562f28e549c22763a2bb1d9b0f
andyl <andy@r210.com>
2026-04-03 21:15:54 -0700

    fix: remove old secrets file

--- config/runtime.exs
+++ config/runtime.exs
@@ -46,7 +46,8 @@ if config_env() == :prod do
       # Set it to  {0, 0, 0, 0, 0, 0, 0, 1} for local network only access.
       # See the documentation on https://hexdocs.pm/bandit/Bandit.html#t:options/0
       # for details about using IPv6 vs IPv4 and loopback vs public addresses.
-      ip: {0, 0, 0, 0, 0, 0, 0, 0}
+      ip: {0, 0, 0, 0, 0, 0, 0, 0},
+      port: String.to_integer(System.get_env("PORT") || "5070")
     ],
     secret_key_base: secret_key_base
 
